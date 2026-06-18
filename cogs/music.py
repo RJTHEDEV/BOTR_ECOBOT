4cde8e08701588335a7d67eb13223162ae41d7de
RJ <103953487+RJTHEDEV@users.noreply.github.com>
2026-06-18 00:58:16 -0400

    Fix yt-dlp bug_reports_message lambda crashing

--- cogs/music.py
+++ cogs/music.py
@@ -4,7 +4,7 @@ import yt_dlp
 import asyncio
 
 # Suppress noise about console usage from errors
-yt_dlp.utils.bug_reports_message = lambda: ''
+yt_dlp.utils.bug_reports_message = lambda *args, **kwargs: ''
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
